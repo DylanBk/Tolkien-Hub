b4fd0d3e57bb66bfa96506422e9390f5dc05fad1
Dylan <d.bullock1404@outlook.com>
2024-09-09 15:00:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template,request, session, url_for
+import os
+import db_functions as db
 
 app = Flask(__name__)
+load_dotenv()
+app.secret_key = os.environ.get('SECRET_KEY')
+
+db_path = './instance/users.db'
+
+
+def get_news():
+    pass
 
 
 # --- ROUTES ---
@@ -9,10 +20,51 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    with open('./news/news.txt', 'r') as f:
+        lines = f.readlines()
+        news_title = lines[0]
+        news_content = lines[1]
+
+    try:
+        news_image = './news/news-image.png'
+    except:
+        news_image = False
+
+    if news_image:
+        return render_template('index.html', news_title=news_title, news_content=news_content, news_image=news_image)
+    else:
+        return render_template('index.html', news_title=news_title, news_content=news_content)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        with db.connect(db_path) as conn:
+            db.create_user(conn, email, username, password, role="User")
+
+        return redirect('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        pass
+    else:
+        return redirect('index.html')
 
 
 # --- MAIN ---
+
+db.create_db(db_path)
+with db.connect(db_path) as conn:
+    pass # add default admin
 
 if __name__ == "__main__":
     app.run()
---
+++ db_functions.py
@@ -0,0 +1,46 @@
+import bcrypt
+import os
+import sqlite3
+
+db_path = './instance/users.db'
+
+
+def connect(db_path):
+    return sqlite3.connect(db_path)
+
+def db_exists(db_path):
+    if os.path.exists(db_path):
+        return True
+    else:
+        return False
+
+def create_table(conn, table_name, columns):
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS {} ({})".format(table_name, ", ".join(columns)))
+    conn.commit()
+
+def create_db(db_path):
+    db = db_exists(db_path)
+
+    if db:
+        print("Database Already Exists")
+    else:
+        print("Creating Database...")
+
+        conn = connect(db_path)
+    
+        conn.execute("PRAGMA foreign_keys = ON")
+
+        create_table(conn, "users", ["uid INTEGER PRIMARY KEY AUTOINCREMENT", "email TEXT NOT NULL", "username TEXT NOT NULL", "password TEXT NOT NULL", "role TEXT DEFAULT 'User'"])
+        conn.close()
+
+def create_user(conn, user_data):
+    email, username, password, role = user_data
+
+    c = conn.cursor()
+
+    salt = bcrypt.gensalt()
+    password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    c.execute("INSERT INTO users (email, username, password, role) VALUES (?, ?, ?, ?)", (email, username, password, role))
+    return c.lastrowid
